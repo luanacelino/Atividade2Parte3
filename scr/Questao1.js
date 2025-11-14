bf8828e9ecf481c61c0868722f5a133c5c68614a
luan23acelino <luan.acelino@academico.ifpb.edu.br>
2025-11-14 17:41:35 -0300

    Update Questao1.js

--- scr/Questao1.js
+++ scr/Questao1.js
@@ -1,1 +1,39 @@
+function formatarProdutos(produtosJSON) {
+  
+  const dados = JSON.parse(produtosJSON);
 
+  const listaFormatada = [];
+
+  dados.itens.forEach(item => {
+    const nome = item.nome;
+    const preco = item.preco.toFixed(2);
+    const categoria = item.categoria;
+
+    const texto = `${nome} - R$ ${preco} (Categoria: ${categoria})`;
+    listaFormatada.push(texto);
+  });
+
+  return listaFormatada;
+}
+
+// --- Teste ---
+const produtosJSON = `{
+ "itens": [
+   {
+     "id": 1,
+     "nome": "Notebook Gamer",
+     "preco": 2999.99,
+     "categoria": "eletronicos",
+     "tags": ["tecnologia", "computacao", "gamer"]
+   },
+   {
+     "id": 2, 
+     "nome": "Mesa Escritório",
+     "preco": 450.50,
+     "categoria": "moveis",
+     "tags": ["escritorio", "madeira", "profissional"]
+   }
+ ]
+}`;
+
+console.log(formatarProdutos(produtosJSON));
